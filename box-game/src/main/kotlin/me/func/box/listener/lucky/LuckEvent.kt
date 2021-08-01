5db41024763e85dc9e02a56e1e1b4a0e0a342fef
reidjJava <sp1.ke@mail.ru>
2021-08-02 01:52:48 +0500

    Update LuckEvent.kt

--- box-game/src/main/kotlin/me/func/box/listener/lucky/LuckEvent.kt
+++ box-game/src/main/kotlin/me/func/box/listener/lucky/LuckEvent.kt
@@ -1,13 +1,40 @@
 package me.func.box.listener.lucky
 
 import me.func.box.User
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack;
 
 enum class LuckEvent(val luckyConsumer: (User) -> Any) {
 
-    TEST({ it.player!!.sendMessage("Что-то происходит") }),;
+    GOLDEN_APPLE({
+        it.player!!.inventory.addItem(dev.implario.bukkit.item.item {
+            type = Material.GOLDEN_APPLE
+        }.build())
+    }),
+    TNT_PRIMED({
+        val tnt = it.player!!.world.spawn(it.player!!.location, org.bukkit.entity.TNTPrimed::class.java)
+        tnt.fuseTicks = 200
+        tnt
+    }),
+    GIVE_TNT({
+        it.player!!.inventory.addItem(dev.implario.bukkit.item.item {
+            type = Material.TNT
+            amount(2)
+        }.build())
+    }),
+    BLOCK_BREAK({
+        val block = it.player!!.location.subtract(0.0, 10.0, 0.0).block
+        if (block?.type != Material.BEDROCK)
+            block.breakNaturally(ItemStack(org.bukkit.Material.AIR))
+    }),
+    GIVE_BED({
+        it.player!!.inventory.addItem(dev.implario.bukkit.item.item {
+            type = Material.BED
+        }.build())
+    }),
+    ;
 
     fun accept(user: User) {
         luckyConsumer(user)
     }
-
 }
