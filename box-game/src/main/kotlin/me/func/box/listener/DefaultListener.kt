f201109d0ee698dc3a90489d088f63d80bf0dcad
DerbYl <soslisqp@gmail.com>
2022-08-18 14:49:11 +0300

    change ui player stat

--- box-game/src/main/kotlin/me/func/box/listener/DefaultListener.kt
+++ box-game/src/main/kotlin/me/func/box/listener/DefaultListener.kt
@@ -146,8 +146,14 @@ class DefaultListener : Listener {
     }
 
     @EventHandler
+<<<<<<< Updated upstream
     fun CraftItemEvent.handle() {
         if(recipe.result.getType() == Material.ANVIL) isCancelled = true
+=======
+    fun PlayerMoveEvent.handle() {
+        if (to.x > app.size + 12 || to.y > app.size || to.z > app.size + 12 || to.x < -12 || to.z < -12)
+            player.health = 0.0
+>>>>>>> Stashed changes
     }
 
     @EventHandler
