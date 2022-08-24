40f89e32728898f633109248dd77897512cf2169
Царюк Артём <42806772+funcid@users.noreply.github.com>
2022-08-25 00:58:04 +0300

    Update LogService.kt

--- service/src/main/kotlin/me.func/battlepass/LogService.kt
+++ service/src/main/kotlin/me.func/battlepass/LogService.kt
@@ -5,7 +5,6 @@ import com.mongodb.client.model.Indexes.hashed
 import com.mongodb.client.model.Sorts
 import me.func.protocol.GetLastLogs
 import me.func.protocol.LogPacket
-import me.func.serviceapi.answer
 import me.func.serviceapi.mongo.MongoAdapter
 import me.func.serviceapi.runListener
 import ru.cristalix.core.GlobalSerializers
@@ -22,7 +21,7 @@ fun main() {
         System.getenv("MONGO_COLLECTION")
     )
 
-    mongo.collection.createIndex(hashed("player")) { _, _ -> }
+    mongo.collection.createIndex(hashed("uuidPlayer")) { _, _ -> }
     mongo.collection.createIndex(hashed("action")) { _, _ -> }
     mongo.collection.createIndex(hashed("data")) { _, _ -> }
     mongo.collection.createIndex(hashed("timestamp")) { _, _ -> }
@@ -30,7 +29,7 @@ fun main() {
     // Начало работы брокера-сообщений
     MicroserviceBootstrap.bootstrap(MicroServicePlatform(2))
 
-    fun findLogPlayer(uuid: UUID) = mongo.collection.find(Filters.eq("player", uuid.toString()))
+    fun findLogPlayer(uuid: UUID) = mongo.collection.find(Filters.eq("uuidPlayer", uuid.toString()))
 
     ISocketClient.get().apply {
         registerCapabilities(
@@ -40,7 +39,7 @@ fun main() {
                 .build()
         )
 
-        runListener<LogPacket>{ realm, pckg ->
+        runListener<LogPacket> { realm, pckg ->
             mongo.save(pckg)
             println("Log ${pckg.uuidPlayer}: ${pckg.action.name} - ${pckg.data}, from ${realm.realmName}")
         }
@@ -52,24 +51,22 @@ fun main() {
                 .sort(Sorts.descending("timestamp"))
                 .limit(total)
                 .forEach({
-                list.add(GlobalSerializers.fromJson(com.mongodb.util.JSON.serialize(it), LogPacket::class.java))
-                          }, { _: Void?, error: Throwable? ->
-                run {
-                    if (error != null) {
-                        error.printStackTrace();
-                        return@run
+                    list.add(GlobalSerializers.fromJson(com.mongodb.util.JSON.serialize(it), LogPacket::class.java))
+                }, { _: Void?, error: Throwable? ->
+                    run {
+                        if (error != null) {
+                            error.printStackTrace();
+                            return@run
+                        }
+                        accept(list)
                     }
-                    accept(list)
-                }
-            })
+                })
         }
 
-        runListener<GetLastLogs>{ realm, pckg ->
+        runListener<GetLastLogs> { realm, pckg ->
             getLastLogs(pckg.player, pckg.count) { lastLog ->
                 println("Send last ${pckg.count} logs to ${pckg.player} from ${realm.realmName}")
-                answer(pckg.apply {
-                    logs = lastLog
-                })
+                forward(realm, pckg.apply { logs = lastLog })
             }
         }
     }
