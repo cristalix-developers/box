9af2a91014f86b244fe1c78c3fa4e5e5d573c9c6
DerbYl <soslisqp@gmail.com>
2022-08-30 13:52:26 +0300

    fix logs menu, refactor code, fix hard quests lucky (BP)

--- box-data/src/main/kotlin/me/func/box/quest/ServerType.kt
+++ box-data/src/main/kotlin/me/func/box/quest/ServerType.kt
@@ -3,6 +3,6 @@ package me.func.box.quest
 enum class ServerType(val title: String, val address: String) {
     BOX1X4("1x4", "BOX4"),
     BOX4X4("4x4", "BOXS"),
-    BOXLUCKY("4x4 lucky", "BOX8"),
+    BOX8("4x4 lucky", "BOX8"),
     ANY("any", "")
 }
